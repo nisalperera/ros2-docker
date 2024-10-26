cdf5f83f93e8522dcd48cf2129063502ec2a9077
Nisal Perera <chinthanapereranisal@gmail.com>
2024-10-26 11:30:04 +0530

    update to foxy distro

--- setup.sh
+++ setup.sh
@@ -10,22 +10,22 @@ sudo apt-get update && sudo apt-get install -y --no-install-recommends \
     sudo rm -rf /var/lib/apt/lists/*
 
 
-echo "Adding Deadsnakes PPA\\n"
+echo "\\nAdding Deadsnakes PPA\\n"
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 
-echo "Install Git, Python and PyQT5 \\n"
+echo "\\nInstall Git, Python and PyQT5 \\n"
 # Install Git, Python and PyQT5
 sudo apt install -y --no-install-recommends git python3-pip python3-pyqt5 libgl1-mesa-glx
 
-echo "Install ros2 packages including Gazebo \\n"
+echo "\\nInstall ros2 packages including Gazebo \\n"
 # Install ros2 packages including Gazebo 
 sudo apt-get -y install ros-${ROS_DISTRO}-ros-gz ros-${ROS_DISTRO}-gazebo-ros-pkgs ros-${ROS_DISTRO}-joint-state-publisher-gui htop
 
-echo "Rosdep update \\n"
+echo "\\nRosdep update \\n"
 # Rosdep update
 rosdep update
 
-echo "Source the ROS setup file and final configs \\n"
+echo "\\nSource the ROS setup file and final configs \\n"
 # Source the ROS setup file
 echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
 echo "export XDG_RUNTIME_DIR=/home/$USER" >> ~/.bashrc
@@ -33,6 +33,6 @@ echo "export PYTHONPATH=$PYTHONPATH:$HOME/.local/lib/python3.10/site-packages"
 echo "export PATH=$PATH:$HOME/.local/lib/python3.10/site-packages"
 echo "chmod 0700 /home/$USER" >> ~/.bashrc
 
-echo "Finalizing setup... \\n"
+echo "\\nFinalizing setup... \\n"
 sudo rm -rf /var/lib/apt/lists/*
 sh $HOME/projects/remove.sh
