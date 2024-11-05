33b5e83fe59ccdbb331ff037d0780f2b9dbaf187
Nisal Perera <nisal.perera@veracitygp.com>
2024-11-05 13:18:48 +0530

    move rosdep update to dockerfile

--- setup.sh
+++ setup.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-. /opt/ros/humble/setup.sh
-
-echo "\\nRosdep update \\n"
-# Rosdep update
-rosdep update
-
-echo "\\nSource the ROS setup file and final configs \\n"
-# Source the ROS setup file
-echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
-echo "export XDG_RUNTIME_DIR=/home/$USER" >> ~/.bashrc
-echo "export PYTHONPATH=$PYTHONPATH:$HOME/.local/lib/python3.10/site-packages"
-echo "chmod 0700 /home/$USER" >> ~/.bashrc
-
 echo "\\nFinalizing setup... \\n"
 sudo rm -rf /var/lib/apt/lists/*
 sh $HOME/projects/remove.sh
