365e275438e07b39bbc6079bfac14f02866cc4d9
Nisal Perera <chinthanapereranisal@gmail.com>
2025-02-02 12:29:21 +0530

    bug fixes in ignition migrations

--- setup.sh
+++ setup.sh
@@ -8,6 +8,5 @@ sudo usermod -a -G realtime $(whoami)
 sudo rm -rf /var/lib/apt/lists/*
 sh $HOME/projects/remove.sh
 
-
 colcon build --packages-select $BUILD_PACKAGES --symlink-install
 echo "source $HOME/projects/install/setup.bash" >> ~/.bashrc
