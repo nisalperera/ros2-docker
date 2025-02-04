0297691a285d9ab5cb5d0a18cde2373f2cd95ef4
Nisal Perera <chinthanapereranisal@gmail.com>
2025-02-04 19:23:31 +0530

    updated dockerfile to use ignition harmony with humble

--- setup.sh
+++ setup.sh
@@ -8,5 +8,7 @@ sudo usermod -a -G realtime $(whoami)
 sudo rm -rf /var/lib/apt/lists/*
 sh $HOME/projects/remove.sh
 
+/bin/bash -c "source /opt/ros/humble/setup.bash"
+
 colcon build --packages-select $BUILD_PACKAGES --symlink-install
 echo "source $HOME/projects/install/setup.bash" >> ~/.bashrc
