ce551f2fe8497448a8ee9985436e752cb463319e
Nisal Perera <chinthanapereranisal@gmail.com>
2025-08-16 15:21:57 +0530

    initial commit

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "\\nFinalizing setup... \\n"
+
+sudo addgroup realtime
+sudo usermod -a -G realtime $(whoami)
+
 sudo rm -rf /var/lib/apt/lists/*
 sh $HOME/projects/remove.sh
+
+colcon build --packages-select $BUILD_PACKAGES --symlink-install
+echo "source $HOME/projects/install/setup.bash" >> ~/.bashrc
